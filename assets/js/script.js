9a38bd9808165def8a144b895f1b41e557b4f921
bryce <blarkin13@gmail.com>
2022-02-05 18:28:25 -0600

    city data

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,32 @@
-var city;
+var weatherContainer = document.getElementById("weather-container");
+var searchButton = document.getElementById("search-button");
+var findCity = document.getElementById("city-search");
 // API Key
-const APIKey = "";
+const APIKey = "e78cada811c8675dd1bb60b68aa5f2d0";
 
-var getWeatherData = function() {
+var getWeatherData = function(city) {
     
-    var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+    var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + APIKey;
 
     fetch(queryURL).then(function(response) {
-        response.json().then(function(data) {
-            displayTemp(data, city);
-            console.log(city);
-        })
+        return response.json()
     })
+    .then(function(data) {
+        console.log(data);
+        console.log(data.main.temp);
+        console.log(data.main.humidity);
+        console.log(data.name);
+        console.log(data.wind.speed);
+        console.log(data.weather[0].icon);
+        var HTML = `<h1>${data.name}<span>${data.weather[0].icon}</span></h1>
+        <h2>Temp: ${data.main.temp}F</h2>
+        <h2>Humidity: ${data.main.humidity}%</h2>`
+        weatherContainer.innerHTML= HTML
+    })
+    
 }
 
-getWeatherData();
+searchButton.addEventListener("click", function() {
+getWeatherData(findCity.value);
+})
+
